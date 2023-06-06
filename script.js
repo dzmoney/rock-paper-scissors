57f31f336c2b2e0cb5f05c6fdc253c66f1fcc873
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-06-06 16:04:49 -0400

    Created getUserChoice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//function getComputerChoice
+//COMPUTER CHOICE
 function getComputerChoice() {
   //define choices
   let choice = ["rock", "paper", "scissors"];
@@ -14,4 +14,28 @@ function getComputerChoice() {
 }
 
 //output compChoice
-console.log(getComputerChoice());
+let computerSelection = getComputerChoice();
+
+//
+//
+//USER CHOICE
+function getUserChoice() {
+  let userInput = prompt("Type: rock, paper, or scissors");
+  //if input, output userSelection
+  switch (userInput.toLowerCase()) {
+    case "rock":
+      return "rock";
+      break;
+    case "paper":
+      return "paper";
+      break;
+    case "scissors":
+      return "scissors";
+      break;
+    default:
+      alert("Invalid answer. Please choose rock, paper, or scissors.");
+      return null;
+  }
+}
+
+let userSelection = getUserChoice();
