9189a0118d5cb4fc33c9b6c0638a9509e91ebabd
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-06-13 16:56:05 -0400

    Reworking in progress

--- script.js
+++ script.js
@@ -1,91 +1,66 @@
-//KEEP SCORE FUNCTIONALITY
+//declare variables
 let userScore = 0;
 let computerScore = 0;
+let maxScore = 5;
 
-function keepScore(result) {
-  if (result && result.startsWith("You win!")) {
-    userScore++;
-  } else if (result && result.startsWith("You lose!")) {
-    computerScore++;
-  }
+function getUserChoice() {
+  let btnRock = document.querySelector("#btn-rock");
+  let btnPaper = document.querySelector("#btn-paper");
+  let btnScissors = document.querySelector("#btn-scissors");
 
-  console.log(`You: ${userScore} \nComputer: ${computerScore}`);
-}
-//
-//
-//
-//report winner at the end
+  btnRock.addEventListener("click", () => {
+    let choice = "rock";
+  });
+  btnPaper.addEventListener("click", () => {
+    let choice = "paper";
+  });
+  btnScissors.addEventListener("click", () => {
+    let choice = "scissors";
+  });
 
-function game() {
-  //PLAY 5 ROUNDS
-  for (let i = 0; i < 5; i++) {}
-
-  //
-  //USER CHOICE
-  function getUserChoice() {
-    let btnRock = document.querySelector("#btn-rock");
-    let btnPaper = document.querySelector("#btn-paper");
-    let btnScissors = document.querySelector("#btn-scissors");
-
-    btnRock.addEventListener("click", () => {
-      userSelection = "rock";
-      playRound();
-    });
-    btnPaper.addEventListener("click", () => {
-      userSelection = "paper";
-      playRound();
-    });
-    btnScissors.addEventListener("click", () => {
-      userSelection = "scissors";
-      playRound();
-    });
-  }
-
-  //
-  //COMPUTER CHOICE
-  function getComputerChoice() {
-    //define choices
-    let choice = ["rock", "paper", "scissors"];
-
-    //create random index based on number of choices
-    //by multiplying Math.random by choice.length (length of array)
-    //we ensure that resulting output is between 0-2 and will
-    //not fall outside range of array options
-    let randomIndex = Math.floor(Math.random() * choice.length);
-
-    //access choice at a random index position
-    return choice[randomIndex];
-  }
-
-  //
-  //
-  //PLAY THE GAME
-  function playRound() {
-    let computerSelection = getComputerChoice();
-
-    console.log(`You chose ${userSelection}`);
-    console.log(`The computer chose ${computerSelection}`);
-
-    let result;
-
-    if (computerSelection === userSelection) {
-      result = "You tie!";
-    } else if (
-      (computerSelection === "rock" && userSelection === "scissors") ||
-      (computerSelection === "scissors" && userSelection === "paper") ||
-      (computerSelection === "paper" && userSelection === "rock")
-    ) {
-      result = `You lose! ${computerSelection} beats ${userSelection}`;
-    } else {
-      result = `You win! ${userSelection} beats ${computerSelection}`;
-    }
-
-    keepScore(result);
-  }
-
-  getUserChoice();
+  choice += userChoice;
+  return userChoice;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  game();
-});
+function getComputerChoice() {
+  let choice = ["rock", "paper", "scissors"];
+
+  //create random index based on number of choices
+  //by multiplying Math.random by choice.length (length of array)
+  //we ensure that resulting output is between 0-2 and will
+  //not fall outside range of array options
+  let randomIndex = Math.floor(Math.random() * choice.length);
+
+  //access choice at a random index position
+  let computerChoice = choice[randomIndex];
+  return computerChoice;
+}
+
+function playRound(userChoice, computerChoice) {
+  if (computerChoice === userChoice) {
+    console.log("You tie!");
+  } else if (
+    (computerChoice === "rock" && userChoice === "scissors") ||
+    (computerChoice === "scissors" && userChoice === "paper") ||
+    (computerChoice === "paper" && userChoice === "rock")
+  ) {
+    console.log(`You lose! ${computerChoice} beats ${userChoice}`);
+  } else {
+    console.log(`You win! ${userChoice} beats ${computerChoice}`);
+  }
+}
+
+// function keepScore(result) {
+//   if (result && result.startsWith("You win!")) {
+//     userScore++;
+//   } else if (result && result.startsWith("You lose!")) {
+//     computerScore++;
+//   }
+
+//   console.log(`You: ${userScore} \nComputer: ${computerScore}`);
+
+//
+//
+//
+//IF NEEDED
+// document.addEventListener("DOMContentLoaded", () => {});
